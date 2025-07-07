d99e52ca218ed483c82a727e1bdcdb5669d91fbe
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2025-07-07 11:49:04 +0530

    minor changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:frontend/features/admin/widgets/side_navigation_bar.dart';
-import 'package:frontend/features/receptionist/Dashboard/screens/receptionist_main.dart';
-=======
->>>>>>> 74f401649530b7ac15cd560fedc76f62c4519cf5
 import 'package:google_fonts/google_fonts.dart';
 import 'features/receptionist/Dashboard/screens/receptionist_main.dart';
 
